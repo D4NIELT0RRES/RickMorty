a426ce10d8dee547cc4bea75f21d38e318c236c8
danieltorres <161612167+D4NIELT0RRES@users.noreply.github.com>
2025-05-13 15:05:11 -0300

    Finalizando trabalho

--- app/src/main/java/br/senai/sp/jandira/rickmorty/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/rickmorty/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.senai.sp.jandira.rickmorty.screens.HomeScreen
 import br.senai.sp.jandira.rickmorty.ui.theme.Rick_and_MortyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Rick_and_MortyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
